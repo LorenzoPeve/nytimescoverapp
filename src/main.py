db4e3d43cca13c6be75802eb14d76eb5dd9da613
Lorenzo Peve <lpeve01@gmail.com>
2023-08-06 10:38:14 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,8 +7,9 @@ load_dotenv()
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-print(list(blob_service_client.list_containers()))
-#container_client = BlobServiceClient.get_container_client('tutorial')
+#print(list(blob_service_client.list_containers()))
+container_client = blob_service_client.get_container_client('tutorial')
 
 
-
+print(container_client)
+print(blob_service_client.account_url)
